f4539167f2ac232fedd94e3e796b7bf8f206e49e
Joshua Pimentel <JRPimentel1417@gmail.com>
2025-06-07 22:57:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Logo = () => {
   return (
     <Link href="/" className="text-2xl font-headline font-bold text-primary hover:text-primary/90 transition-colors">
-      Throne Advisor
+      Restroom Reviews
     </Link>
   );
 };
